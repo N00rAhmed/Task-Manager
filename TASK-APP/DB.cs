d35e962315f1cf18e88f83d798b51214b86070ea
N00rAhmed <tronn232003@gmail.com>
2023-10-18 13:05:48 +0100

    small change

--- TASK-APP/DB.cs
+++ TASK-APP/DB.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Npgsql;
-namespace TASK_APP
+/*using Npgsql;
+*/namespace TASK_APP
 {
     public class DB
     {
