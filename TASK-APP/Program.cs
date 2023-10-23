f36f3900c934a464959c9620a9000e161e679f27
N00rAhmed <tronn232003@gmail.com>
2023-10-23 13:01:22 +0100

    made small changes for deployment

--- TASK-APP/Program.cs
+++ TASK-APP/Program.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.IO;
 using System.Reflection;
-/*using Npgsql;
-*/namespace TASK_APP
+using System.Windows.Forms;
+
+namespace TASK_APP
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
@@ -23,13 +19,46 @@ using System.Reflection;
 
         static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
-            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("TASK_APP.Npgsql.dll"))
+            // Define a dictionary to map assembly names to resource names
+            Dictionary<string, string> assemblyResourceMap = new Dictionary<string, string>
             {
-                byte[] assemblyData = new byte[stream.Length];
-                stream.Read(assemblyData, 0, assemblyData.Length);
-                return Assembly.Load(assemblyData);
+                { "Npgsql", "TASK_APP.Npgsql.dll" },
+                { "System.Threading.Tasks.Extensions", "TASK_APP.System.Threading.Tasks.Extensions.dll" },
+                { "Microsoft.Bcl.AsyncInterfaces", "TASK_APP.Microsoft.Bcl.AsyncInterfaces.dll" },
+                { "Microsoft.Extensions.Logging.Abstractions", "TASK_APP.Microsoft.Extensions.Logging.Abstractions.dll" },
+                { "System.Memory", "TASK_APP.System.Memory.dll" },
+                { "System.Runtime.CompilerServices.Unsafe", "TASK_APP.System.Runtime.CompilerServices.Unsafe.dll" },
+                { "System.Runtime", "TASK_APP.System.Runtime.dll" },
+                { "System.Numerics.Vectors", "TASK_APP.System.Numerics.Vectors.dll" },
+                { "System.Threading.Channels", "TASK_APP.System.Threading.Channels.dll" },
+                { "netstandard", "TASK_APP.netstandard.dll" },
+                { "System.Threading.Tasks.Parallel", "TASK_APP.System.Threading.Tasks.Parallel.dll" },
+                { "System.Threading.Tasks", "TASK_APP.System.Threading.Tasks.dll" }
+/*System.Runtime.dll*/
+/*System.Runtime*/
+/*System.Runtime.CompilerServices.Unsafe*/
+/*'System.Memory, */
+/*Microsoft.Extensions.Logging.Abstractions.dll*/
+                // Add other assemblies and their resource names as needed
+            };
 
+            // Get the requested assembly name
+            string assemblyName = new AssemblyName(args.Name).Name;
+
+            // Check if the assembly is in the mapping
+            if (assemblyResourceMap.ContainsKey(assemblyName))
+            {
+                // Load the assembly from the embedded resource
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(assemblyResourceMap[assemblyName]))
+                {
+                    byte[] assemblyData = new byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
             }
+
+            // Return null if the assembly is not found in the mapping
+            return null;
         }
     }
 }
